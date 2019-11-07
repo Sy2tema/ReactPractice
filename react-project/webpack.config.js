4e1ca350b37665b488ab20fa09bbc4ab3896ffd0
Sy2tema <bood00142@gmail.com>
2019-11-07 21:20:39 +0900

    Add: 끝말잇기 프로젝트 생성
    class방식을 사용해 React를 이용한 프로젝트를 작성했습니다.

--- react-project/webpack.config.js
+++ react-project/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
     name: 'wordchain-game',
@@ -8,22 +9,28 @@ module.exports = {
         extensions: ['.js', '.jsx'],
     }, //resolve의 extensions옵션에 원하는 확장자들을 넣어주면 웹팩이 알아서 해당 확장자로 된 파일이 있는지 찾아준다.
     
-    //입력과 출력에 대한 설정은 가장 중요한 부분중 하나이다.
+    //입력과 모듈, 출력에 대한 설정은 가장 중요한 부분중 하나이다.
     entry: {
         app: ['./client']
     }, //입력 설정
-
     module: {
         rules: [{
             test: /\.jsx?/, //정규 표현식 문법. .js와 .jsx확장자를 찾는다는 뜻이다.
             loader: 'babel-loader',
             options: {
-                presets: ['@babel/preset-env', '@babel/preset-react'],
+                //플러그인들의 모음을 presets라고 부른다.
+                presets: [['@babel/preset-env', {
+                    targets: {
+                        browsers: ['> 5% in KR'] //babel이 지원할 브라우저 버전을 설정할 수 있다.
+                    } //>5% in KR이라 설정한다면 한국에서 브라우저 점유율이 5%를 넘는 브라우저들을 전부 지원한다는 뜻이다.
+                }], '@babel/preset-react'],
                 plugins: ['@babel/plugin-proposal-class-properties'] //state를 사용하기 위해 필요한 모듈
             }, //Babel의 옵션들을 넣어준다.
         }]
     }, //합치기로 한 파일들에 모듈을 적용한다.
-
+    plugins: [
+        new webpack.LoaderOptionsPlugin({debug: true}),
+    ], //모듈에 적용한 플러그인 외의 추가적으로 사용하고자 하는 외부 플러그인들을 설정한다.
     output: {
         path: path.join(__dirname, 'dist'), //현재 폴더 경로에 입력한 경로를 합쳐준다.
         filename: 'app.js'
---
+++ wordchain-game/WordChainGame.jsx
@@ -0,0 +1,70 @@
+//사용을 원하는 모듈은 reqire메소드를 이용해 요청한 후 불러온다.
+const React = require('react');
+const {Component} = React;
+
+//클래스 형식으로 작성한 코드
+class WordChainGame extends Component {
+    state = {
+        word: '자동차',
+        inputValue: '',
+        isCorrect: ''
+    };
+
+    onSubmitForm = (e) => {
+        e.preventDefault();
+
+        if (this.inputValue === '')
+            return;
+
+        if (regexr.test(this.inputValue)) {
+            this.setState({
+                isCorrect: '숫자나 특수문자는 들어갈 수 없습니다. 단어를 다시 입력해주세요.',
+                inputValue: ''
+            });
+            this.input.focus();
+
+            return;
+        }
+
+        if (this.state.word[this.state.word.length - 1] === this.state.inputValue[0]) {
+            this.setState({
+                isCorrect: '형식에 잘 맞게 작성하셨습니다.',
+                word: this.state.inputValue,
+                inputValue: ''
+            });
+        } else {
+            this.setState({
+                isCorrect: '제시된 글자의 가장 뒤 글자와 입력할 단어의 제일 앞 단어를 일치시켜주세요.',
+                inputValue: ''
+            });
+        }
+
+        this.input.focus();
+    };
+
+    onChangeInput = (e) => {
+        this.setState({inputValue: e.target.value});
+    };
+
+    input;
+    
+    inputRef = (target) => {
+        this.input = target;
+    };
+
+    render() {
+        return (
+            <>
+                <div>현재 단어 : {this.state.word}</div>
+                <form onSubmit={this.onSubmitForm}>
+                    <input type="text" ref={this.inputRef} value={this.state.inputValue} onChange={this.onChangeInput}/>
+                    <button>입력</button>
+                </form>
+                <div>{this.state.isCorrect}</div>
+            </>
+        );
+    };
+}
+
+//다른 파일에서 이 컴포넌트를 불러올 수 있도록 export작업을 수행한다.
+module.exports = WordChainGame;
