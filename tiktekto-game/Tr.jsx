2d333f1b20042d585fd80052ba94ad593b30ff84
Sy2tema <bood00142@gmail.com>
2019-11-24 06:12:17 +0900

    Update: 렌더링 효율 개선
    Tr에도 memo를 적용해 렌더링 효율을 개선했습니다.

--- tiktekto-game/Tr.jsx
+++ tiktekto-game/Tr.jsx
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, {memo} from 'react';
 import Td from './Td';
 
-const Tr = ({rowData, rowIndex, dispatch}) => {
+//반복문이 존재하는 컴포넌트에 주로 memo를 적용하면 효과를 볼 수 있다.
+//React.memo로도 효과를 보지 못할 경우 useMemo를 활용해 특정 컴포넌트를 기억하도록 만들 수도 있다.
+const Tr = memo(({rowData, rowIndex, dispatch}) => {
     return (
         <tr>
             {Array(rowData.length).fill().map((td, index) => (<Td dispatch={dispatch} rowIndex={rowIndex} cellIndex={index} cellData={rowData[index]}>{''}</Td>))}
         </tr>
     );
-};
+});
 
 export default Tr;
